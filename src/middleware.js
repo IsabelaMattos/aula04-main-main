ad973c7afb3200b157e131bd99b00e98f5a4ee90
IsabelaMattos <isabela.paula2@estudante.ifms.edu.br>
2023-09-14 17:10:53 -0400

    configurei as rotas

--- src/middleware.js
+++ src/middleware.js
@@ -6,16 +6,24 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c', request.url);
+    const lista = new URL('./pages/dashboard', request.url); 
     const isTokenValidated = validateToken(token);
 
     if (!isTokenValidated || !token) {
-        if (request.nextUrl.pathname === '/pages/dashboard') {
+        if (request.nextUrl.pathname === '/pages/dashboard' ||
+        request.nextUrl.pathname === '/pages/alterar' || request.nextUrl.pathname === '/pages/registrar'
+        ) {
             return NextResponse.redirect(urlLogin);
+        }
+    }
+    if (isTokenValidated) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(Lista);
         }
     }
     NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard']
+    matcher: ['/', '/pages/dashboard', '/pages/alterar', '/pages/registrar']
 };
 
