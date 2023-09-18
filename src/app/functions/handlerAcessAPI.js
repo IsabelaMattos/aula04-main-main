98c637871fdfaf068bdbeb1338fa38c18aded4d8
IsabelaMattos <isabela.paula2@estudante.ifms.edu.br>
2023-09-18 18:24:58 -0400

    arrumei o código novamente, pq não esta dando certo

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,45 +1,46 @@
 'use server'
 
-const lista=[{
+const lista = [
+    {
     name:'Isabela',
-    password:'123',
     email:'isabela@gmail.com',
+    password:'123',
     token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 },
 
 {
     name:'Taty',
-    password:'123',
     email:'taty@gmail.com',
+    password:'123',
     token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 },
 
 {
     name:'Bruna',
-    password:'123',
     email:'bruna@gmail.com',
+    password:'123',
     token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
 },
 
 {
     name:'Vini',
-    password:'123',
     email:'vini@gmail.com',
+    password:'123',
     token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-}]
+    }
+]
 
 const getUserAuthenticated = (user) => {
-    let listaUser = {};
-
-    lista.map ((lista => {
-        if(lista.email === lista.email & lista.password === lista.password){
-            listaUser = lista;
+    let userAuth = {};
+for ( let i = 0; i < users.length; i++){ 
+        if(users[i].email === lista.email && users[i].password === user.password){
+            return userAuth = users[i];
         }
-    }))
-    return listaUser;
+    }
+    return userAuth;
 }
 
 const getUsers = () =>{
-        return lista;
+    return users;
 }
 export { getUsers, getUserAuthenticated };
